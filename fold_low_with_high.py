a5a1ef4e27ffae74c00efde26c4bf150389e7bc3
cjchallis <christopher.challis@gmail.com>
2015-02-03 06:22:30 -0500

    Example tourney.
    Added comments to hopefully make Strategies and running a tournament
    clearer.

--- fold_low_with_high.py
+++ fold_low_with_high.py
@@ -1,10 +1,13 @@
 class FoldLowWithHigh:
     def __init__(self, fold, hand):
+        ''' Strategy that folds when 'fold' is available if 'hand' or higher is in hand'''
         self.fold = fold
         self.hand = hand
 
     def play(self, info):
+        # get best fold as tuple (playerIndex, card)
         best = info.bestFold()
+        # get current hand
         stack = self.player.stack
         if best[1] <= self.fold and max(stack) >= self.hand:
             return best
--- tourney.py
+++ tourney.py
@@ -1,16 +1,22 @@
 import sys
+# this should be the path to pairsClasses.py
+# not needed if all files are in the same folder
 sys.path.append('C:/Users/Challis/Chris/Python/pairsTournament')
+# import pairsClasses and all Strategies to be played
 import pairsClasses as p
 from fold_low_with_high import FoldLowWithHigh
 from expectation import Expectation
 
 losers = []
-for i in range(50000):
-    d = p.Dealer()
+for i in range(100):
+    # defaults to 5 players that always hit
+    d = p.Dealer(4)
+    # replace some strategies
     d.gameState.players[0].strategy = FoldLowWithHigh(3, 8)
     d.gameState.players[1].strategy = FoldLowWithHigh(4, 8)
     d.gameState.players[2].strategy = Expectation()
+    # play a game and append the loser index
     losers.append(d.play())
 
-for i in range(6):
-    print(losers.count(i))
+for i in range(1, len(d.gameState.players)+1):
+    print("Player " + str(i) + ": " + str(losers.count(i)))
